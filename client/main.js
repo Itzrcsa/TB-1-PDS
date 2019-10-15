4eb893d6f9c95ad3ae9fe21242ee10eac0ec9954
Robson Carlos Santos Ayres <junioryuri197@gmail.com>
2019-10-15 14:43:17 -0400

    Talvez funcione agora

--- client/main.js
+++ client/main.js
@@ -1,2 +1,5 @@
-//import '../imports/ui/body.js';
-import '../imports/api/tasks.js';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
